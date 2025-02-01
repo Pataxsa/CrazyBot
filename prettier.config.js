0e4a1a09d0106ce81a7bc2300f35d96a1e24607a
0BL1V10N <cvjeticaxel@gmail.com>
2025-02-01 18:13:28 +0100

    Add typescript support

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    tabWidth: 4,
-    printWidth: 120,
-    singleQuote: false,
-    bracketSpacing: true,
-    arrowParens: "avoid",
-    trailingComma: "none",
-    semi: true
-};
